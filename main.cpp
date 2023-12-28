61156386cdeb775fa79a7cfa89b4c6f3da4bae84
BobBrunkov <asanoverlan2005@gmail.com>
2023-12-28 18:52:02 +0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+using namespace std;
+
+int main()
+{
+    cout << "Hello Worlds";
+
+    return 0;
+}
